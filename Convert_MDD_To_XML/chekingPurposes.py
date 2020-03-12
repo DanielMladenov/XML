adabfbb62e55792c63359a3975a56faadde9409f
DanielMladenov <39006236+DanielMladenov@users.noreply.github.com>
2020-03-12 13:18:35 +0200

    AddingGridsIntoXml

--- Convert_MDD_To_XML/chekingPurposes.py
+++ Convert_MDD_To_XML/chekingPurposes.py
@@ -10,13 +10,16 @@ for MddTree in root.iter('variable'):
     #Flat_Grid = MddTree.attrib.values()
     flat_Grid = MddTree.get('level')
     print(flat_Grid)
-    for ques in MddTree.iter('name'):
-        print(ques.text)
-    for title in MddTree.iter('title'):
-        for titleText in title.iter('text'):
-            print(titleText.text)
-    for categories in MddTree.iter('categories'):
-        for value in categories.iter('value'):
-            print(value.text)
-            for valueText in value.iter('text'):
-                print(valueText.text)
+    if flat_Grid == '0': #This is for the Flat Questions
+        for ques in MddTree.iter('name'):
+            print(ques.text)
+        for title in MddTree.iter('title'):
+            for titleText in title.iter('text'):
+                print(titleText.text)
+        for categories in MddTree.iter('categories'):
+            for value in categories.iter('value'):
+                print(value.text)
+                for valueText in value.iter('text'):
+                    getLanguage = valueText.get('lang')
+                    if getLanguage == 'en-US':
+                        print(valueText.text)
