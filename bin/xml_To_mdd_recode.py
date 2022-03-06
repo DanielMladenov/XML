902ef9180da9bbe412f3389f672cfeffe8110db8
DanielMladenov <DanielPlamenovMladenov@gmail.com>
2022-03-06 16:31:37 +0200

    Update xml_To_mdd_recode.py

--- bin/xml_To_mdd_recode.py
+++ bin/xml_To_mdd_recode.py
@@ -1,3 +1,4 @@
+from turtle import left
 import xml.etree.ElementTree as ET
 import re
 
@@ -22,24 +23,44 @@ class xmlImport(object):
                 
         self.listQuesLen = len(self.listQues)
 
-    def checkQuestionName(self): #compare the current question name to the previous and next question in order to find out if it's part of a group.
-        for i in range(1, self.listQuesLen):
-            if i != self.listQuesLen - 1:
-                print(self.listQues[i-1], self.listQues[i], self.listQues[i+1])
-                #return [self.listQues[i-1], self.listQues[i], self.listQues[i+1]]
-            else:
-                print(self.listQues[i-1], self.listQues[i])
-                #return [self.listQues[i-1], self.listQues[i]]
+    def quesNameArr(self, i): #compare the current question name to the previous and next question in order to find out if it's part of a group.
+        if i != self.listQuesLen - 1:
+           #print(self.listQues[i-1], self.listQues[i], self.listQues[i+1])
+            return [self.listQues[i-1], self.listQues[i], self.listQues[i+1]]
+        else:
+            #print(self.listQues[i-1], self.listQues[i])
+            return [self.listQues[i-1], self.listQues[i]]
 
     def getNoFlatQuestionMatrix(self):
         for i in range(1, self.listQuesLen):
             if self.listQues[i].find(self.standardDelimiter) != -1:
-                self.matrix.append([0,1,0])
+                firstPosition = self.getNoFlatQuestionFirstPosition( self.quesNameArr(i), self.listQues[i].find(self.standardDelimiter))
+                midPosition = self.getNoFlatQuestionMidPosition()
+                lastPosition = self.getFlatQuestionLastPosition()
+                self.matrix.append([firstPosition,midPosition,lastPosition])
             else:
                 pass
                 self.matrix.append([0,0,0])
 
+    def getNoFlatQuestionFirstPosition(self, arr,delimPosition):
+        if len(arr) == 3:
+            if arr[1][:delimPosition] == arr[0][:delimPosition]:  #check if the current quesName (left from the delim) match with the previous question names
+                return 1
+            elif arr[1][:delimPosition] == arr[2][:delimPosition]:    #check if the current quesName (left from the delim) match with the next question name
+                return 1
+            else:
+                return 0
+        elif len(arr) == 2:
+            if arr[1][:delimPosition] == arr[0][:delimPosition]:  #check if the current quesName (left from the delim) match with the previous question name
+                return 1
+        else:
+            print("Array of question names has unexpected len (number of elements). Number of elements should be 2 or 3")
 
+    def getNoFlatQuestionMidPosition(self):
+        return 1
+    
+    def getFlatQuestionLastPosition(self):
+        return 0
 
 
 
@@ -51,7 +72,7 @@ class xmlImport(object):
 #==================================
 
 #xmlTest = xmlImport('../docs/12375e01_liv.xml')
-xmlTest = xmlImport(r'C:\Users\Danie\Documents\GitHub\XML\docs\147736_SurveyData.xml')
+xmlTest = xmlImport(r'docs\147736_SurveyData.xml')
 xmlTest.getLayout()
 xmlTest.getNoFlatQuestionMatrix()
 #xmlTest.printMyQues()
